adffe4afcee768e24d20166e944528120266529c
Sevo98 <sevo98.sk@gmail.com>
2021-09-23 04:01:47 +0700

    Reduced size created

---
+++ GMCAD/GMCAD/MainForm.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace GMCAD
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveUsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trackBarHeight = new System.Windows.Forms.TrackBar();
+            this.trackBarWidth = new System.Windows.Forms.TrackBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.menuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarHeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarWidth)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(855, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.openToolStripMenuItem.Text = "Open image";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem1,
+            this.saveUsToolStripMenuItem});
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // saveToolStripMenuItem1
+            // 
+            this.saveToolStripMenuItem1.Name = "saveToolStripMenuItem1";
+            this.saveToolStripMenuItem1.Size = new System.Drawing.Size(122, 22);
+            this.saveToolStripMenuItem1.Text = "Save";
+            // 
+            // saveUsToolStripMenuItem
+            // 
+            this.saveUsToolStripMenuItem.Name = "saveUsToolStripMenuItem";
+            this.saveUsToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.saveUsToolStripMenuItem.Text = "Save us...";
+            // 
+            // trackBarHeight
+            // 
+            this.trackBarHeight.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBarHeight.Location = new System.Drawing.Point(63, 30);
+            this.trackBarHeight.Maximum = 100;
+            this.trackBarHeight.Minimum = 1;
+            this.trackBarHeight.Name = "trackBarHeight";
+            this.trackBarHeight.Size = new System.Drawing.Size(406, 45);
+            this.trackBarHeight.TabIndex = 2;
+            this.trackBarHeight.Value = 100;
+            this.trackBarHeight.Scroll += new System.EventHandler(this.trackBarHeight_Scroll);
+            // 
+            // trackBarWidth
+            // 
+            this.trackBarWidth.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.trackBarWidth.Location = new System.Drawing.Point(12, 78);
+            this.trackBarWidth.Maximum = 100;
+            this.trackBarWidth.Minimum = 1;
+            this.trackBarWidth.Name = "trackBarWidth";
+            this.trackBarWidth.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBarWidth.Size = new System.Drawing.Size(45, 406);
+            this.trackBarWidth.TabIndex = 3;
+            this.trackBarWidth.Value = 100;
+            this.trackBarWidth.Scroll += new System.EventHandler(this.trackBarWidth_Scroll);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.pictureBox);
+            this.panel1.Location = new System.Drawing.Point(63, 78);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(406, 406);
+            this.panel1.TabIndex = 4;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(855, 510);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.trackBarWidth);
+            this.Controls.Add(this.trackBarHeight);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "MainForm";
+            this.Text = "Raster image viewer";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarHeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarWidth)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem saveUsToolStripMenuItem;
+        private System.Windows.Forms.TrackBar trackBarHeight;
+        private System.Windows.Forms.TrackBar trackBarWidth;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ GMCAD/GMCAD/MainForm.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GMCAD
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private Image image;
+        private int width;
+        private int height;
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = "Images|*.bmp;*.png;*.jpg|All files|*.*";
+            if (openDialog.ShowDialog() != DialogResult.OK)
+            {
+            }
+
+            try
+            {
+                image = new Bitmap(openDialog.FileName);
+                width = image.Width;
+                height = image.Height;
+                pictureBox.Image = image;
+
+            }
+            catch (OutOfMemoryException)
+            {
+                MessageBox.Show("Unsupported format!");
+                return;
+            }
+            
+        }
+
+        private void trackBarHeight_Scroll(object sender, EventArgs e)
+        {
+            if (image == null)
+            {
+                MessageBox.Show("Select image!");
+                trackBarHeight.Value = 100;
+                return;
+            }
+
+            height = (image.Height * trackBarHeight.Value) / 100;
+            pictureBox.Image = ResizeNow(width, height);
+
+            
+
+        }
+
+        private void trackBarWidth_Scroll(object sender, EventArgs e)
+        {
+            if (image == null)
+            {
+                MessageBox.Show("Select image!");
+                trackBarWidth.Value = 100;
+                return;
+            }
+
+            width = (image.Width * trackBarWidth.Value) / 100;
+            pictureBox.Image = ResizeNow(width, height);
+
+        }
+
+        private Bitmap ResizeNow(int target_width, int target_height)
+        {
+            Rectangle dest_rect = new Rectangle(0, 0, target_width, target_height);
+            Bitmap destImage = new Bitmap(target_width, target_height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+            using (var g = Graphics.FromImage(destImage))
+            {
+                g.CompositingMode = CompositingMode.SourceCopy;
+                g.CompositingQuality = CompositingQuality.HighQuality;
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.SmoothingMode = SmoothingMode.HighQuality;
+                g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                using (var wrapmode = new ImageAttributes())
+                {
+                    wrapmode.SetWrapMode(WrapMode.TileFlipXY);
+                    g.DrawImage(image, dest_rect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapmode);
+                }
+            }
+
+            return destImage;
+        }
+    }
+}
